a79b152dc2f9207b35bcf3153d70962c7b21465d
Nikodemus Siivola <nikodemus@random-state.net>
2019-05-31 11:33:25 +0300

    split lib.rs

---
+++ src/syntax.rs
@@ -0,0 +1,46 @@
+#[derive(Debug, PartialEq)]
+pub struct Identifier(pub String);
+
+#[derive(Debug, PartialEq)]
+pub enum Literal {
+    Integer(i64),
+    Float(f64),
+    Character(String),
+    Symbol(String),
+    String(String),
+    Array(Vec<Literal>),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Method {
+    pub pattern: Pattern,
+    pub temporaries: Vec<Identifier>,
+    pub statements: Vec<Expr>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Pattern {
+    Unary(Identifier),
+    Binary(Identifier, Identifier),
+    Keyword(Identifier, Identifier, Option<Box<Pattern>>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Cascade {
+    Unary(Identifier),
+    Binary(Identifier, Expr),
+    Keyword(Identifier, Expr, Option<Box<Cascade>>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expr {
+    Constant(Literal),
+    Variable(Identifier),
+    Unary(Box<Expr>, Identifier),
+    Binary(Box<Expr>, Identifier, Box<Expr>),
+    Keyword(Box<Expr>, Vec<Identifier>, Vec<Expr>),
+    Assign(Identifier, Box<Expr>),
+    Return(Box<Expr>),
+    Block(Vec<Identifier>, Vec<Expr>),
+    Cascade(Box<Expr>, Vec<Cascade>),
+}
