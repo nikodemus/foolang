d951949744c4e5fb33c7bfcdac803bc30121b74d
Nikodemus Siivola <nikodemus@random-state.net>
2019-06-01 10:13:20 +0300

    eval symbol works

--- src/objects.rs
+++ src/objects.rs
@@ -7,12 +7,16 @@ pub enum Object {
     Float(f64),
     Character(Arc<String>),
     String(Arc<String>),
+    Symbol(Arc<String>),
     Block(Arc<ast::Block>),
 }
 
 impl Object {
     pub fn make_string(s: &str) -> Object {
         Object::String(Arc::new(String::from(s)))
+    }
+    pub fn make_symbol(s: &str) -> Object {
+        Object::Symbol(Arc::new(String::from(s)))
     }
     pub fn make_char(s: &str) -> Object {
         let s = String::from(s);
