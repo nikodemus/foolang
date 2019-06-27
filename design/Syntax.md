22c6d6a443b8e5f491aeb4ed1216674966deb31d
Nikodemus Siivola <nikodemus@random-state.net>
2019-06-28 00:23:19 +0300

    wip: new scanner, annotations

---
+++ design/Syntax.md
@@ -0,0 +1,9 @@
+How much whitespace?
+
+Ie. is foo<bar legal?
+
+If it is legal, then foo <bar should be too.
+
+...and then lexing quickly becomes ambiguos.
+
+Allow basic math ops without whitespace?
--- src/stream.rs
+++ src/stream.rs
@@ -2,6 +2,7 @@ type Span = (usize, usize);
 
 #[derive(Debug, PartialEq)]
 enum Token {
+    Annotation(Span),
     Character(Span),
     CloseBrace(Span),
     CloseBracket(Span),
@@ -31,6 +32,19 @@ impl SyntaxError {
 
 const DELIMITERS: [char; 6] = ['(', ')', '{', '}', '[', ']'];
 
+fn scan_str_part(s: &str) -> Result<Token, SyntaxError> {
+    StringStream::new(s).scan()
+}
+
+fn scan_str(s: &str) -> Vec<Result<Token, SyntaxError>> {
+    let mut stream = StringStream::new(s);
+    let mut result = vec![];
+    while !stream.at_eof() {
+        result.push(stream.scan());
+    }
+    result
+}
+
 trait Stream {
     fn scan(&mut self) -> Result<Token, SyntaxError> {
         let mut start;
@@ -38,27 +52,37 @@ trait Stream {
             if self.at_eof() {
                 return Ok(Token::Eof((self.len(), self.len())));
             }
-            start = self.getchar();
+            start = self.getchar()?;
             if !start.1.is_whitespace() {
                 break;
             }
         }
+        match start.1 {
+            '\'' => return self.scan_character(start.0),
+            '<' => return self.scan_annotation_or_sigil(start.0),
+            _ => {}
+        }
         let alphanumeric = start.1.is_alphanumeric();
         let mut keyword = start.1 == ':';
-        let mut end = self.getchar();
+        let mut end = self.getchar()?;
         loop {
-            if end.1.is_whitespace() || self.at_eof() {
+            if end.1.is_whitespace() {
+                break;
+            }
+            if self.at_eof() {
+                end.0 = self.len();
                 break;
             }
             if end.1 == ':' {
                 keyword = true;
             } else if alphanumeric != end.1.is_alphanumeric() {
+                self.unread(end);
                 break;
             }
             if DELIMITERS.iter().find(|x| **x == end.1).is_some() {
                 break;
             }
-            end = self.getchar();
+            end = self.getchar()?;
         }
         let span = (start.0, end.0);
         if keyword {
@@ -88,134 +112,159 @@ trait Stream {
             Ok(Token::LocalId(span))
         }
     }
-    /*
-    fn scan_character(&mut self) -> Result<Token, SyntaxError> {
-        let start = self.position();
-        let mut indices = self.str()[start..].char_indices();
-        indices.next();
-        if indices.next().is_none() {
-            return Err(SyntaxError::new((start, start), "Unexpected end of file"));
+    fn scan_annotation_or_sigil(&mut self, start: usize) -> Result<Token, SyntaxError> {
+        let next = self.getchar()?;
+        if next.1.is_whitespace() || next.1.is_digit(10) {
+            self.unread(next);
+            return Ok(Token::Sigil((start, next.0)));
         }
-        let end = match indices.next() {
-            None => return Err(SyntaxError::new((start, start), "Unexpected end of file")),
-            Some((pos, mark)) => {
-                if mark != '\'' {
-                    return Err(SyntaxError::new(
-                        (start, start + pos),
-                        "Malformed character literal",
-                    ));
-                }
-                self.seek(start + pos + 1)
+        loop {
+            let next = self.getchar()?;
+            if next.1 == '>' {
+                return Ok(Token::Annotation((start, next.0 + 1)));
             }
-        };
-        Ok(Token::Character((start, end)))
+        }
     }
-    */
+    fn scan_character(&mut self, start: usize) -> Result<Token, SyntaxError> {
+        self.getchar()?;
+        let (end, quote) = self.getchar()?;
+        if quote != '\'' {
+            return Err(SyntaxError::new(
+                (start, end),
+                "Malformed character literal",
+            ));
+        }
+        Ok(Token::Character((start, end + 1)))
+    }
     // --
     fn at_eof(&self) -> bool;
-    fn getchar(&mut self) -> (usize, char);
+    fn unread(&mut self, result: (usize, char));
+    fn getchar(&mut self) -> Result<(usize, char), SyntaxError>;
     fn len(&self) -> usize;
-}
-
-fn scan_str(s: &str) -> Result<Token, SyntaxError> {
-    StringStream::new(s).scan()
 }
 
 #[test]
 fn scan_eof() {
-    assert_eq!(scan_str("   "), Ok(Token::Eof((3, 3))));
+    assert_eq!(scan_str_part("   "), Ok(Token::Eof((3, 3))));
 }
 
-#[ignore]
 #[test]
 fn scan_char() {
-    assert_eq!(scan_str(" 'x' "), Ok(Token::Character((1, 4))));
+    assert_eq!(scan_str_part(" 'x' "), Ok(Token::Character((1, 4))));
+    assert_eq!(scan_str_part("'x'"), Ok(Token::Character((0, 3))));
 }
 
 #[test]
 fn scan_local_id() {
-    assert_eq!(scan_str(" fo1 "), Ok(Token::LocalId((1, 4))));
+    assert_eq!(scan_str_part(" fo1 "), Ok(Token::LocalId((1, 4))));
+    assert_eq!(scan_str_part("fo1"), Ok(Token::LocalId((0, 3))));
 }
 
 #[test]
 fn scan_local_id2() {
-    assert_eq!(scan_str(" fo1+ "), Ok(Token::LocalId((1, 4))));
+    assert_eq!(scan_str_part(" fo1+ "), Ok(Token::LocalId((1, 4))));
+}
+
+#[test]
+fn scan_binary_op() {
+    assert_eq!(
+        scan_str(" foo++bar "),
+        vec![
+            Ok(Token::LocalId((1, 4))),
+            Ok(Token::Sigil((4, 6))),
+            Ok(Token::LocalId((6, 9))),
+        ]
+    )
+}
+
+#[test]
+fn scan_annotations() {
+    assert_eq!(
+        scan_str("foo<Foo>+bar<Bar>"),
+        vec![
+            Ok(Token::LocalId((0, 3))),
+            Ok(Token::Annotation((3, 8))),
+            Ok(Token::Sigil((8, 9))),
+            Ok(Token::LocalId((9, 12))),
+            Ok(Token::Annotation((12, 17))),
+        ]
+    )
 }
 
 #[test]
 fn scan_global_id() {
-    assert_eq!(scan_str(" Fo1 "), Ok(Token::GlobalId((1, 4))));
+    assert_eq!(scan_str_part(" Fo1 "), Ok(Token::GlobalId((1, 4))));
 }
 
 #[test]
 fn scan_global_id2() {
-    assert_eq!(scan_str(" Fo1+ "), Ok(Token::GlobalId((1, 4))));
+    assert_eq!(scan_str_part(" Fo1+ "), Ok(Token::GlobalId((1, 4))));
 }
 
 #[test]
 fn scan_decimal() {
-    assert_eq!(scan_str(" 1xx "), Ok(Token::Number((1, 4))))
+    assert_eq!(scan_str_part(" 1xx "), Ok(Token::Number((1, 4))))
 }
 
 #[test]
 fn scan_sigil() {
-    assert_eq!(scan_str(" + "), Ok(Token::Sigil((1, 2))))
+    assert_eq!(scan_str_part(" + "), Ok(Token::Sigil((1, 2))))
 }
 
 #[test]
 fn scan_sigil2() {
-    assert_eq!(scan_str(" +foo "), Ok(Token::Sigil((1, 2))))
+    assert_eq!(scan_str_part(" +foo "), Ok(Token::Sigil((1, 2))))
 }
 
 #[test]
 fn scan_keyword() {
-    assert_eq!(scan_str(" foo: "), Ok(Token::Keyword((1, 5))))
+    assert_eq!(scan_str_part(" foo: "), Ok(Token::Keyword((1, 5))))
 }
 
 #[test]
 fn scan_keyword2() {
-    assert_eq!(scan_str(" : "), Ok(Token::Keyword((1, 2))))
+    assert_eq!(scan_str_part(" : "), Ok(Token::Keyword((1, 2))))
 }
 
 #[test]
 fn scan_keyword3() {
-    assert_eq!(scan_str(" ::: "), Ok(Token::Keyword((1, 4))))
+    assert_eq!(scan_str_part(" ::: "), Ok(Token::Keyword((1, 4))))
 }
 
 #[test]
 fn scan_open_paren() {
-    assert_eq!(scan_str(" ((  "), Ok(Token::OpenParen((1, 2))))
+    assert_eq!(scan_str_part(" ((  "), Ok(Token::OpenParen((1, 2))))
 }
 
 #[test]
 fn scan_close_paren() {
-    assert_eq!(scan_str(" )) "), Ok(Token::CloseParen((1, 2))))
+    assert_eq!(scan_str_part(" )) "), Ok(Token::CloseParen((1, 2))))
 }
 
 #[test]
 fn scan_open_brace() {
-    assert_eq!(scan_str(" {{ "), Ok(Token::OpenBrace((1, 2))))
+    assert_eq!(scan_str_part(" {{ "), Ok(Token::OpenBrace((1, 2))))
 }
 
 #[test]
 fn scan_close_brace() {
-    assert_eq!(scan_str(" }} "), Ok(Token::CloseBrace((1, 2))))
+    assert_eq!(scan_str_part(" }} "), Ok(Token::CloseBrace((1, 2))))
 }
 
 #[test]
 fn scan_open_bracket() {
-    assert_eq!(scan_str(" [[ "), Ok(Token::OpenBracket((1, 2))))
+    assert_eq!(scan_str_part(" [[ "), Ok(Token::OpenBracket((1, 2))))
 }
 
 #[test]
 fn scan_close_bracket() {
-    assert_eq!(scan_str(" ]] "), Ok(Token::CloseBracket((1, 2))))
+    assert_eq!(scan_str_part(" ]] "), Ok(Token::CloseBracket((1, 2))))
 }
 
 struct StringStream<'a> {
     source: &'a str,
     indices: std::cell::RefCell<std::str::CharIndices<'a>>,
-    cache: std::cell::RefCell<Option<(usize, char)>>,
+    cache: std::cell::RefCell<Vec<(usize, char)>>,
 }
 
 impl<'a> StringStream<'a> {
@@ -223,7 +272,7 @@ impl<'a> StringStream<'a> {
         StringStream {
             source,
             indices: std::cell::RefCell::new(source.char_indices()),
-            cache: std::cell::RefCell::new(None),
+            cache: std::cell::RefCell::new(Vec::new()),
         }
     }
 }
@@ -233,21 +282,27 @@ impl<'a> Stream for StringStream<'a> {
         self.source.len()
     }
     fn at_eof(&self) -> bool {
+        if !self.cache.borrow().is_empty() {
+            return false;
+        }
         match self.indices.borrow_mut().next() {
             None => return true,
-            Some(place) => {
-                assert_eq!(None, self.cache.replace(Some(place)));
+            Some(result) => {
+                self.cache.borrow_mut().push(result);
                 return false;
             }
         }
     }
-    fn getchar(&mut self) -> (usize, char) {
-        if let Some((pos, c)) = self.cache.replace(None) {
-            return (pos, c);
+    fn unread(&mut self, result: (usize, char)) {
+        self.cache.borrow_mut().push(result)
+    }
+    fn getchar(&mut self) -> Result<(usize, char), SyntaxError> {
+        if let Some(cached) = self.cache.borrow_mut().pop() {
+            return Ok(cached);
         }
         self.indices
             .borrow_mut()
             .next()
-            .expect("Unexpected EOF in StringStream::getchar()")
+            .ok_or(SyntaxError::new((self.len(), self.len()), "Unexpected EOF"))
     }
 }
