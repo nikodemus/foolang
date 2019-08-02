5adf308b85fc4ac63e5180d5aa43113d72eddb64
Nikodemus Siivola <nikodemus@random-state.net>
2019-08-02 14:53:17 +0300

    add forgotten files

---
+++ src/classes/boolean2.rs
@@ -0,0 +1,47 @@
+use crate::objects2::{Eval, Foolang, Object, Vtable};
+
+pub fn vtable() -> Vtable {
+    let mut vt = Vtable::new("Boolean");
+    vt.def("ifFalse:", boolean_if_false);
+    vt.def("ifTrue:", boolean_if_true);
+    vt.def("ifTrue:ifFalse:", boolean_if_true_if_false);
+    vt.def("not", boolean_not);
+    vt.def("toString", boolean_to_string);
+    vt
+}
+
+fn boolean_if_true(receiver: &Object, args: &[&Object], foo: &Foolang) -> Eval {
+    if receiver.boolean() {
+        args[0].send("value", &[], foo)
+    } else {
+        Ok(receiver.clone())
+    }
+}
+
+fn boolean_if_false(receiver: &Object, args: &[&Object], foo: &Foolang) -> Eval {
+    if receiver.boolean() {
+        Ok(receiver.clone())
+    } else {
+        args[0].send("value", &[], foo)
+    }
+}
+
+fn boolean_if_true_if_false(receiver: &Object, args: &[&Object], foo: &Foolang) -> Eval {
+    if receiver.boolean() {
+        args[0].send("value", &[], foo)
+    } else {
+        args[1].send("value", &[], foo)
+    }
+}
+
+fn boolean_not(receiver: &Object, _args: &[&Object], foo: &Foolang) -> Eval {
+    Ok(foo.make_boolean(!receiver.boolean()))
+}
+
+fn boolean_to_string(receiver: &Object, _args: &[&Object], foo: &Foolang) -> Eval {
+    if receiver.boolean() {
+        Ok(foo.make_string("True"))
+    } else {
+        Ok(foo.make_string("False"))
+    }
+}
---
+++ src/unwind.rs
@@ -0,0 +1,194 @@
+use crate::eval::Frame;
+use crate::objects2::Object;
+use crate::tokenstream::Span;
+
+#[derive(PartialEq, Debug)]
+pub enum Unwind {
+    Exception(Error, Location),
+    ReturnFrom(Frame, Object),
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Error {
+    SimpleError(SimpleError),
+    TypeError(TypeError),
+}
+
+#[derive(PartialEq, Debug)]
+pub struct SimpleError {
+    pub what: &'static str,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct TypeError {
+    pub value: Object,
+    pub expected: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Location {
+    pub span: Option<Span>,
+    pub context: Option<String>,
+}
+
+impl Unwind {
+    // FIXME: The vtable as expected, extract name here.
+    pub fn type_error<T>(value: Object, expected: String) -> Result<T, Unwind> {
+        Err(Unwind::Exception(
+            Error::TypeError(TypeError {
+                value,
+                expected,
+            }),
+            Location::none(),
+        ))
+    }
+    pub fn type_error_at<T>(span: Span, value: Object, expected: String) -> Result<T, Unwind> {
+        Err(Unwind::Exception(
+            Error::TypeError(TypeError {
+                value,
+                expected,
+            }),
+            Location::new(span),
+        ))
+    }
+    pub fn error_at<T>(span: Span, what: &'static str) -> Result<T, Unwind> {
+        Err(Unwind::Exception(
+            Error::SimpleError(SimpleError {
+                what,
+            }),
+            Location::new(span),
+        ))
+    }
+    pub fn return_from<T>(frame: Frame, value: Object) -> Result<T, Unwind> {
+        Err(Unwind::ReturnFrom(frame, value))
+    }
+    pub fn add_span(&mut self, span: &Span) {
+        if let Unwind::Exception(_, location) = self {
+            location.add_span(span)
+        }
+    }
+    pub fn with_context(mut self, source: &str) -> Unwind {
+        if let Unwind::Exception(error, location) = &mut self {
+            location.add_context(source, error.what());
+        }
+        self
+    }
+}
+
+impl Error {
+    pub fn what(&self) -> String {
+        match self {
+            Error::SimpleError(e) => e.what(),
+            Error::TypeError(e) => e.what(),
+        }
+    }
+}
+
+impl SimpleError {
+    fn what(&self) -> String {
+        self.what.to_string()
+    }
+}
+
+impl TypeError {
+    fn what(&self) -> String {
+        format!(
+            "{} expected, got: {} {}",
+            self.expected,
+            self.value.vtable.name.clone(),
+            self.value
+        )
+    }
+}
+
+impl Location {
+    fn new(span: Span) -> Location {
+        Location {
+            span: Some(span),
+            context: None,
+        }
+    }
+
+    fn none() -> Location {
+        Location {
+            span: None,
+            context: None,
+        }
+    }
+
+    pub fn context(&self) -> String {
+        match &self.context {
+            None => "".to_string(),
+            Some(ctx) => ctx.clone(),
+        }
+    }
+
+    fn start(&self) -> usize {
+        if let Some(span) = &self.span {
+            span.start
+        } else {
+            panic!("Expected Location with span")
+        }
+    }
+
+    fn end(&self) -> usize {
+        if let Some(span) = &self.span {
+            span.end
+        } else {
+            panic!("Expected Location with span")
+        }
+    }
+
+    fn add_span(&mut self, span: &Span) {
+        assert!(self.span.is_none());
+        self.span = Some(span.clone())
+    }
+
+    fn add_context(&mut self, source: &str, what: String) {
+        assert!(self.span.is_some());
+        assert!(self.context.is_none());
+        let mut context = String::new();
+        let mut prev = "";
+        let mut lineno = 1;
+        let mut start = 0;
+        for line in source.lines() {
+            if start >= self.end() {
+                // Line after the problem -- done.
+                _append_context_line(&mut context, lineno, line);
+                break;
+            }
+            let end = start + line.len();
+            if end > self.start() {
+                // Previous line if there is one.
+                if lineno > 1 {
+                    _append_context_line(&mut context, lineno - 1, prev);
+                }
+                // Line with the problem.
+                _append_context_line(&mut context, lineno, line);
+                let mut mark = if self.start() > start {
+                    String::from_utf8(vec![b' '; self.start() - start]).unwrap()
+                } else {
+                    "".to_string()
+                };
+                mark.push_str(
+                    String::from_utf8(vec![b'^'; self.end() - self.start()]).unwrap().as_str(),
+                );
+                mark.push_str(" ");
+                mark.push_str(what.as_str());
+                _append_context_line(&mut context, 0, mark.as_str());
+            }
+            prev = line;
+            start = end + 1;
+            lineno += 1;
+        }
+        self.context = Some(context);
+    }
+}
+
+fn _append_context_line(context: &mut String, lineno: usize, line: &str) {
+    if lineno == 0 {
+        context.push_str(format!("    {}\n", line).as_str());
+    } else {
+        context.push_str(format!("{:03} {}\n", lineno, line).as_str());
+    }
+}
