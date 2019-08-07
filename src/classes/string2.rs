870a9b5e892eb34c1678f6a7dfc637e4644334ce
Nikodemus Siivola <nikodemus@random-state.net>
2019-08-07 14:26:58 +0300

    remove redundant pubs from string cass

--- src/classes/string2.rs
+++ src/classes/string2.rs
@@ -13,15 +13,15 @@ pub fn class_vtable() -> Vtable {
     vt
 }
 
-pub fn class_string_new(_receiver: &Object, _args: &[Object], foo: &Foolang) -> Eval {
+fn class_string_new(_receiver: &Object, _args: &[Object], foo: &Foolang) -> Eval {
     Ok(foo.make_string(""))
 }
 
-pub fn string_append(receiver: &Object, args: &[Object], foo: &Foolang) -> Eval {
+fn string_append(receiver: &Object, args: &[Object], foo: &Foolang) -> Eval {
     args[0].send("appendToString:", &[receiver.clone()], foo)
 }
 
-pub fn string_append_to_string(receiver: &Object, args: &[Object], foo: &Foolang) -> Eval {
+fn string_append_to_string(receiver: &Object, args: &[Object], foo: &Foolang) -> Eval {
     let mut s = args[0].string_as_str().to_string();
     s.push_str(receiver.string_as_str());
     Ok(foo.into_string(s))
