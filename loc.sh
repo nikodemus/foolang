0bd780b872681eb7fab355143cc5699027e79594
Nikodemus Siivola <nikodemus@random-state.net>
2019-08-29 09:09:16 +0300

    account loc for tests separately

--- loc.sh
+++ loc.sh
@@ -8,7 +8,13 @@ git ls-files \
     | grep -E '\.rs$' \
     | grep -vE '^src/tests|^tests|^src/bin/bench.rs$' \
     | xargs wc -l \
-    | awk 'END { print "Rust      " $1 }'
+    | awk 'END { print "Rust code " $1 }'
+
+git ls-files \
+    | grep -E '\.rs$' \
+    | grep -E '^src/tests|^tests|^src/bin/bench.rs$' \
+    | xargs wc -l \
+    | awk 'END { print "Rust test " $1 }'
 
 git ls-files \
     | grep -E '.foo$' \
