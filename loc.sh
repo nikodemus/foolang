951798d5db902c4332f1da8279ef205d6fa07bb8
Nikodemus Siivola <nikodemus@random-state.net>
2019-08-30 15:17:44 +0300

    fix loc.sh

--- loc.sh
+++ loc.sh
@@ -8,13 +8,13 @@ git ls-files \
     | grep -E '\.rs$' \
     | grep -vE '^src/tests|^tests|^src/bin/bench.rs$' \
     | xargs wc -l \
-    | awk 'END { print "Rust code " $1 }'
+    | awk 'END { print "Rust/code " $1 }'
 
 git ls-files \
     | grep -E '\.rs$' \
     | grep -E '^src/tests|^tests|^src/bin/bench.rs$' \
     | xargs wc -l \
-    | awk 'END { print "Rust test " $1 }'
+    | awk 'END { print "Rust/test " $1 }'
 
 git ls-files \
     | grep -E '.foo$' \
