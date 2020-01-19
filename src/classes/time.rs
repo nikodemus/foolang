8cbd83e343814a74769ccaff7b60807dfbff32fa
Nikodemus Siivola <nikodemus@random-state.net>
2020-01-19 22:40:42 +0200

    move derived Time methods to prelude

--- src/classes/time.rs
+++ src/classes/time.rs
@@ -9,8 +9,6 @@ pub fn instance_vtable() -> Vtable {
     let mut vt = Vtable::new("Time");
     vt.def("addTime:", time_add_time);
     vt.def("subTime:", time_sub_time);
-    vt.def("-", time_sub);
-    vt.def("+", time_add);
     vt.def("real", time_real);
     vt.def("system", time_system);
     vt.def("user", time_user);
@@ -25,14 +23,6 @@ fn time_sub_time(receiver: &Object, args: &[Object], env: &Env) -> Eval {
 fn time_add_time(receiver: &Object, args: &[Object], env: &Env) -> Eval {
     let res = **args[0].time() + **receiver.time();
     Ok(env.foo.make_time(res))
-}
-
-fn time_sub(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("subTime:", std::slice::from_ref(receiver), env)
-}
-
-fn time_add(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("addTime:", std::slice::from_ref(receiver), env)
 }
 
 fn time_real(receiver: &Object, _args: &[Object], env: &Env) -> Eval {
