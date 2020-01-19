2f6b7069e135dd3c857abd39a7952d5b0dd068bf
Nikodemus Siivola <nikodemus@random-state.net>
2020-01-19 20:45:11 +0200

    move derived Integer methods to prelude

--- src/classes/integer.rs
+++ src/classes/integer.rs
@@ -1,11 +1,8 @@
-use std::slice;
-
 use crate::eval::Env;
 use crate::objects::{Eval, Object, Vtable};
 
 pub fn vtable() -> Vtable {
     let mut vt = Vtable::new("Integer");
-    // FUNDAMENTAL
     vt.def("asFloat", integer_as_float);
     vt.def("asInteger", integer_as_integer);
     vt.def("addInteger:", integer_add_integer);
@@ -22,28 +19,8 @@ pub fn vtable() -> Vtable {
     vt.def("to:", integer_to);
     vt.def("to:do:", integer_to_do);
     vt.def("times:", integer_times);
-    // DERIVED
-    vt.def("/", integer_div);
-    vt.def("==", integer_equal);
-    vt.def(">", integer_greater_than);
-    vt.def(">=", integer_greater_than_or_equal);
-    vt.def("<", integer_less_than);
-    vt.def("<=", integer_less_than_or_equal);
-    vt.def("*", integer_mul);
-    vt.def("-", integer_sub);
-    vt.def("addFloat:", integer_add_float);
-    vt.def("divFloat:", integer_div_float);
-    vt.def("mulFloat:", integer_mul_float);
-    vt.def("subFloat:", integer_sub_float);
-    vt.def("equalFloat:", integer_equal_float);
-    vt.def("lessThanFloat:", integer_less_than_float);
-    vt.def("lessThanOrEqualFloat:", integer_less_than_or_equal_float);
-    vt.def("greaterThanFloat:", integer_greater_than_float);
-    vt.def("greaterThanOrEqualFloat:", integer_greater_than_or_equal_float);
     vt
 }
-
-// FUNDAMENTAL METHODS
 
 fn integer_as_integer(receiver: &Object, _args: &[Object], _env: &Env) -> Eval {
     Ok(receiver.to_owned())
@@ -141,74 +118,4 @@ fn integer_to_do(receiver: &Object, args: &[Object], env: &Env) -> Eval {
 
 fn integer_to_string(receiver: &Object, _args: &[Object], env: &Env) -> Eval {
     Ok(env.foo.make_string(&receiver.integer().to_string()))
-}
-
-// DERIVED METHODS
-
-fn integer_div(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("divInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_equal(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("equalInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_greater_than(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("lessThanInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_greater_than_or_equal(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("lessThanOrEqualInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_less_than(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("greaterThanInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_less_than_or_equal(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("greaterThanOrEqualInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_mul(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("mulInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_sub(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    args[0].send("subInteger:", slice::from_ref(receiver), env)
-}
-
-fn integer_add_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("addFloat:", args, env)
-}
-
-fn integer_div_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("divFloat:", args, env)
-}
-
-fn integer_mul_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("mulFloat:", args, env)
-}
-
-fn integer_sub_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("subFloat:", args, env)
-}
-
-fn integer_equal_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("equalFloat:", args, env)
-}
-
-fn integer_greater_than_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("greaterThanFloat:", args, env)
-}
-
-fn integer_greater_than_or_equal_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("greaterThanOrEqualFloat:", args, env)
-}
-
-fn integer_less_than_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("lessThanFloat:", args, env)
-}
-
-fn integer_less_than_or_equal_float(receiver: &Object, args: &[Object], env: &Env) -> Eval {
-    integer_as_float(receiver, &[], env)?.send("lessThanOrEqualFloat:", args, env)
 }
