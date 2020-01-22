5f4f409815727dcd3a8b79664c777624f3934bec
Nikodemus Siivola <nikodemus@random-state.net>
2020-01-23 00:37:16 +0200

    move Output newline to prelude

--- src/classes/output.rs
+++ src/classes/output.rs
@@ -4,7 +4,6 @@ use crate::objects::{Eval, Object, Vtable};
 pub fn vtable() -> Vtable {
     let mut vt = Vtable::new("Output");
     vt.def("flush", output_flush);
-    vt.def("newline", output_newline);
     vt.def("print:", output_print);
     vt.def("toString", output_to_string);
     vt
@@ -12,11 +11,6 @@ pub fn vtable() -> Vtable {
 
 fn output_flush(receiver: &Object, _args: &[Object], _env: &Env) -> Eval {
     receiver.output().flush();
-    Ok(receiver.clone())
-}
-
-fn output_newline(receiver: &Object, _args: &[Object], _env: &Env) -> Eval {
-    receiver.output().write("\n");
     Ok(receiver.clone())
 }
 
