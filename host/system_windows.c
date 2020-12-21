51cd9fa37dbf1322b702efd9586b31c648eef93b
Nikodemus Siivola <nikodemus@random-state.net>
2020-12-21 16:55:32 +0200

    same System#timeSeconds epoch on Windows and Unix

--- host/system_windows.c
+++ host/system_windows.c
@@ -1,14 +1,28 @@
 #include "system.h"
-#include <Windows.h>
-#include <inttypes.h>
 
-union FileTime {
-  FILETIME as_struct;
-  uint64_t as_int;
-};
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <stdint.h>
+#include <winsock2.h>
 
 double system_time_seconds(void) {
-  union FileTime t;
-  GetSystemTimeAsFileTime(&t.as_struct);
-  return t.as_int / 1e7;
+  // Matches the Unix epoch!
+  //
+  // Adapted from: https://stackoverflow.com/questions/10905892/equivalent-of-gettimeday-for-windows
+  //
+  // Note: some broken versions only have 8 trailing zero's, the correct epoch
+  // has 9 trailing zero's This magic number is the number of 100 nanosecond
+  // intervals since January 1, 1601 (UTC) until 00:00:00 January 1, 1970
+  static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+  SYSTEMTIME  system_time;
+  FILETIME    file_time;
+  uint64_t    time;
+
+  GetSystemTime(&system_time);
+  SystemTimeToFileTime(&system_time, &file_time);
+  time =  (uint64_t)file_time.dwLowDateTime;
+  time += (uint64_t)file_time.dwHighDateTime << 32;
+
+  return (time - EPOCH) / 10000000.0 + system_time.wMilliseconds / 1000.0;
 }
