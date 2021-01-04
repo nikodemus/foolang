89f388a2fc9da74a90b386e2683f0524f8739f19
Nikodemus Siivola <nikodemus@random-state.net>
2021-01-04 18:33:39 +0200

    support both windows and unix in debug.sh

--- debug.sh
+++ debug.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ -z "$WINDIR" ]; then
+    ostype="unix"
+else
+    ostype="windows"
+fi
+
 set -euo pipefail
+
 clang-format -i host/generated_*
+
 clang \
     -o tmp_transpile_test.exe \
     -fsanitize=address -fsanitize=undefined \
     -fno-omit-frame-pointer -g -Wall --std=c11 \
-    host/main.c host/system_windows.c ext/dtoa.c
+    host/main.c host/system_$ostype.c ext/dtoa.c
 ./tmp_transpile_test.exe
