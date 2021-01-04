2942e51a55cab24556c99454da3a10430cfcf285
Nikodemus Siivola <nikodemus@random-state.net>
2021-01-04 18:56:23 +0200

    Time#user:system:real: in bootstrap host

--- src/classes/time.rs
+++ src/classes/time.rs
@@ -1,8 +1,11 @@
 use crate::eval::Env;
 use crate::objects::{Eval, Object, Vtable};
+use crate::time::TimeInfo;
 
 pub fn class_vtable() -> Vtable {
-    Vtable::for_class("Time")
+    let vt = Vtable::for_class("Time");
+    vt.add_primitive_method_or_panic("user:system:real:", class_time_user_system_real_);
+    vt
 }
 
 pub fn instance_vtable() -> Vtable {
@@ -11,6 +14,14 @@ pub fn instance_vtable() -> Vtable {
     vt.add_primitive_method_or_panic("system", time_system);
     vt.add_primitive_method_or_panic("user", time_user);
     vt
+}
+
+fn class_time_user_system_real_(_receiver: &Object, args: &[Object], env: &Env) -> Eval {
+    Ok(env.foo.make_time(TimeInfo {
+        user: args[0].float(),
+        system: args[1].float(),
+        real: args[2].float(),
+    }))
 }
 
 fn time_real(receiver: &Object, _args: &[Object], env: &Env) -> Eval {
