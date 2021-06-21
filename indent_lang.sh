68810e18163b21009d63a185c379c2f326a7c9be
Nikodemus Siivola <nikodemus@random-state.net>
2021-06-21 22:29:26 +0300

    indent_lang.sh for canonicalizing indentation in foo/lang
      ...or rather testing SyntaxPrinter using foo/lang.

---
+++ indent_lang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+git checkout foo/lang/
+
+F=${1:-any.foo}
+
+while true
+do
+    cargo run -- foo/format.foo -- foo/lang/$F || break
+    git diff --exit-code foo/lang/$F || break
+    F=$(ls -1 foo/lang/ | grep -A1 ^$F | tail -n 1)
+done
