3e801b1f76d207c8213baf2f1c4ebf8aec1e8cb9
Nikodemus Siivola <nikodemus@random-state.net>
2021-06-29 22:46:12 +0300

    FooInstance_alloc as parallel to foo_mark_instance

--- host/main.c
+++ host/main.c
@@ -286,6 +286,7 @@ struct FooClass FooClass_Selector;
 struct FooClass FooClass_String;
 struct FooPointerList FooClassInheritance_Class;
 struct FooArray* FooArray_alloc(size_t size);
+struct FooArray* FooArray_instance(size_t size);
 struct Foo foo_Float_new(double f);
 struct Foo foo_Integer_new(int64_t n);
 struct Foo foo_String_new(size_t len, const char* s);
@@ -903,6 +904,13 @@ struct FooProcessTimes* FooProcessTimes_new(double user, double system, double r
 }
 
 struct FooArray* FooArray_alloc(size_t size) {
+  struct FooArray* array = foo_alloc(sizeof(struct FooArray) + size*sizeof(struct Foo));
+  array->gc = true;
+  array->size = size;
+  return array;
+}
+
+struct FooArray* FooInstance_alloc(size_t size) {
   struct FooArray* array = foo_alloc(sizeof(struct FooArray) + size*sizeof(struct Foo));
   array->gc = true;
   array->size = size;
