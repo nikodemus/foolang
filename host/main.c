35e887b6eed5660586fe13977d1b9f9200ca8824
Nikodemus Siivola <nikodemus@random-state.net>
2021-07-02 13:23:37 +0300

    trap writes to static arrays

--- host/main.c
+++ host/main.c
@@ -499,6 +499,12 @@ struct Foo foo_class_typecheck(struct FooContext* ctx,
              class->name->data, obj.class->name->data);
 }
 
+void foo_check_modification(struct FooContext* ctx, struct FooHeader* header) {
+  if (header->allocation == STATIC) {
+    foo_panicf(ctx, "Cannot modify constant object!");
+  }
+}
+
 struct Foo foo_Boolean_new(bool t);
 
 struct Foo foo_class_includes(struct FooContext* ctx,
