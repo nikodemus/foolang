b23a532de5003e09f7910820290919025e24a508
Nikodemus Siivola <nikodemus@random-state.net>
2021-07-12 19:54:33 +0300

    script to bootstrap a self-hosted fooc executable
      Also copy the generated code from each build to
      bootstrap-c/ and target-c/ respectively for later
      examination and comparison.
    
      I want 'foo' executable to both be a compiler,
      interpreter, and repl, but since this one just
      compiles I guess calling it fooc makes sense?

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -z "$WINDIR" ]; then
+    EXT=""
+else
+    EXT=".exe"
+fi
+
+# This tricks clang into using the internal symbolizer, leaving path
+# empty leaves the addresses unsymbolized.
+export ASAN_SYMBOLIZER_PATH=0
+
+BOOTSTRAP_COMPILER=./bootstrap-fooc$EXT
+TARGET_COMPILER=./fooc$EXT
+
+echo "Building $BOOTSTRAP_COMPILER..."
+time cargo run -- foo/compile.foo -- foo/compile.foo $BOOTSTRAP_COMPILER
+rm -rf bootstrap-c
+cp -a c bootstrap-c
+echo "$BOOTSTRAP_COMPILER built!"
+
+echo "Building $TARGET_COMPILER..."
+time $BOOTSTRAP_COMPILER foo/compile.foo $TARGET_COMPILER
+rm -f target-c
+cp -a c target-c
+echo "$TARGET_COMPILER built!"
