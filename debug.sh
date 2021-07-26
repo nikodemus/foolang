df7e6d130eaac9f4c5bed037c8c2b29fc55ec8d5
Nikodemus Siivola <nikodemus@random-state.net>
2021-07-26 17:23:50 +0300

    explicit -lm (asan causes it to be silently linked)
      Plus fixing debug.sh for Linux

--- debug.sh
+++ debug.sh
@@ -8,17 +8,15 @@ fi
 
 set -euo pipefail
 
-# This tricks clang into using the internal symbolizer, leaving path
-# empty leaves the addresses unsymbolized.
-export ASAN_SYMBOLIZER_PATH=0
+export ASAN_SYMBOLIZER_PATH=llvm-symbolizer
 
 rm -f tmp_transpile_test.exe
 
 function stacksize_options() {
     # 4MB stack: the parser is highly recursive!
     local size=0x400000
-    if [ -z "$WINDIR" ]; then
-        echo -Wl,-stack-size -W,$size
+    if [ $ostype = "unix" ]; then
+        echo -Wl,-z,-stack-size=$size
     else
         echo -Wl,/STACK:$size
     fi
@@ -28,7 +26,7 @@ clang \
     -o tmp_transpile_test.exe \
     $(stacksize_options) \
     -fsanitize=address -fsanitize=undefined \
-    -fno-omit-frame-pointer -g -Wall --std=c11 \
+    -lm -fno-omit-frame-pointer -g -Wall --std=c11 \
     c/main.c c/system_$ostype.c ext/dtoa.c
 echo "Built, running!"
 time ./tmp_transpile_test.exe "$@"
