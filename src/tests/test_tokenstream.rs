b5c8c9330e7c17a9bdb9cf2a50ad7c623a8c2cdc
Nikodemus Siivola <nikodemus@random-state.net>
2022-10-17 10:23:46 +0300

    tests: move tokenization_tests.json to tests/

--- src/tests/test_tokenstream.rs
+++ src/tests/test_tokenstream.rs
@@ -4,7 +4,7 @@ use crate::tokenstream::*;
 fn run_test_vectors() {
     use serde_json;
     use std::fs;
-    let tests = fs::read_to_string("tokenization_tests.json")
+    let tests = fs::read_to_string("tests/tokenization_tests.json")
         .expect("Could not read tokenization_tests.json");
     let tests: serde_json::Value = serde_json::from_str(tests.as_str()).unwrap();
     let tests = tests.as_array().unwrap();
